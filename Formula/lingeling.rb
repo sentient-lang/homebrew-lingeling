0c6580fbe7d164e53ea64cd4e2e35fb1232d373f
Chris Patuzzo <chris@patuzzo.co.uk>
2016-08-07 15:41:01 +0100

    Update to use SAT competition's 2016 Lingeling

--- Formula/lingeling.rb
+++ Formula/lingeling.rb
@@ -1,8 +1,8 @@
 class Lingeling < Formula
   desc "SAT solver"
   homepage "http://fmv.jku.at/lingeling/"
-  url "http://fmv.jku.at/lingeling/lingeling-bal-2293bef-151109.tar.gz"
-  sha256 "211be9debb67caef07829601b2ea059dbe120470bf33fba6ef79d248baf940a8"
+  url "http://fmv.jku.at/lingeling/lingeling-bbc-9230380-160707.tar.gz"
+  sha256 "7960c69ebd3da1400b0f3135fa08d71abd405c180fc52f785b35ad8a58585126"
 
   def install
     system "./configure.sh"
